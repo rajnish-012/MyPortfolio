ddf2158edff44c8b0e37baffd3cbeece20e282f2
Rajnish Kumar <rajnishkr.22.11.48@gmail.com>
2026-01-01 00:23:54 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
     const formData = new FormData(form);
 
     try {
-      const response = await fetch("paste ur formspree link here", {
+      const response = await fetch("https://formspree.io/f/meeqwybp", {
         method: "POST",
         body: formData,
         headers: { Accept: "application/json" },
